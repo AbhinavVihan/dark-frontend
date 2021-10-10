d904e48290bbf66218962f67e907b6e6d39210dc
Abi31882 <abhinavchaudhary348@gmail.com>
2021-10-10 11:01:50 +0530

    solved the products images issue

--- src/reducers/entity.reducer.ts
+++ src/reducers/entity.reducer.ts
@@ -1,8 +1,25 @@
 import { Entity } from "../models/Entity";
+import { Product } from "../models/Products";
+import { ProductsState } from "./products.reducer";
 
 export interface EntityState<T extends Entity = Entity> {
   byId: {
     [id: string]: T;
+  };
+  imageCover?: {
+    [id: string]: string;
+  };
+  imageFront?: {
+    [id: string]: string;
+  };
+  image1?: {
+    [id: string]: string;
+  };
+  image2?: {
+    [id: string]: string;
+  };
+  image3?: {
+    [id: string]: string;
   };
 }
 
@@ -27,5 +44,107 @@ export const addMany = (state: EntityState, entities: Entity[]) => {
     ...state,
 
     byId: { ...state.byId, ...entityMap },
+  };
+};
+
+export const addImgFront = (state: ProductsState, entities: Product[]) => {
+  if (entities.length === 0) {
+    return state;
+  }
+
+  const entityMap = entities.reduce((prev, entity) => {
+    const img = entity.imageFront;
+
+    return {
+      ...prev,
+      [entity._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+    };
+  }, {});
+
+  return {
+    ...state,
+    imageFront: { ...state.byId, ...entityMap },
+  };
+};
+
+export const addImgCover = (state: ProductsState, entities: Product[]) => {
+  if (entities.length === 0) {
+    return state;
+  }
+
+  const entityMap = entities.reduce((prev, entity) => {
+    const img = entity.imageCover;
+
+    return {
+      ...prev,
+      [entity._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+    };
+  }, {});
+
+  return {
+    ...state,
+
+    imageCover: { ...state.imageCover, ...entityMap },
+  };
+};
+
+export const addImg1 = (state: ProductsState, entities: Product[]) => {
+  if (entities.length === 0) {
+    return state;
+  }
+
+  const entityMap = entities.reduce((prev, entity) => {
+    const img = entity.images[0];
+
+    return {
+      ...prev,
+      [entity._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+    };
+  }, {});
+
+  return {
+    ...state,
+
+    image1: { ...state.image1, ...entityMap },
+  };
+};
+
+export const addImg2 = (state: ProductsState, entities: Product[]) => {
+  if (entities.length === 0) {
+    return state;
+  }
+
+  const entityMap = entities.reduce((prev, entity) => {
+    const img = entity.images[1];
+    return {
+      ...prev,
+      [entity._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+    };
+  }, {});
+
+  return {
+    ...state,
+
+    image2: { ...state.image2, ...entityMap },
+  };
+};
+
+export const addImg3 = (state: ProductsState, entities: Product[]) => {
+  if (entities.length === 0) {
+    return state;
+  }
+
+  const entityMap = entities.reduce((prev, entity) => {
+    const img = entity.images[2];
+    return {
+      ...prev,
+      [entity._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+    };
+  }, {});
+
+  return {
+    ...state,
+
+    image3: { ...state.image3, ...entityMap },
   };
 };
--- src/reducers/products.reducer.ts
+++ src/reducers/products.reducer.ts
@@ -5,7 +5,16 @@ import {
   PRODUCTS_QUERY_COMPLETED,
 } from "../actions/action.constants";
 import { Product } from "../models/Products";
-import { addMany, EntityState, getIds } from "./entity.reducer";
+import {
+  addImg1,
+  addImg2,
+  addImg3,
+  addImgCover,
+  addImgFront,
+  addMany,
+  EntityState,
+  getIds,
+} from "./entity.reducer";
 
 export interface ProductsState extends EntityState<Product> {
   query: string;
@@ -16,6 +25,11 @@ const initialState: ProductsState = {
   byId: {},
   query: "",
   queryMap: {},
+  imageCover: {},
+  imageFront: {},
+  image1: {},
+  image2: {},
+  image3: {},
 };
 
 export const productReducer: Reducer<ProductsState> = (
@@ -31,12 +45,55 @@ export const productReducer: Reducer<ProductsState> = (
 
       const newState = addMany(state, products) as ProductsState;
 
+      const imgCover = products.reduce((prev, product) => {
+        const img = product.imageCover;
+
+        return {
+          ...prev,
+          [product._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+        };
+      }, {});
+      const imgFront = products.reduce((prev, product) => {
+        const img = product.imageFront;
+        return {
+          ...prev,
+          [product._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+        };
+      }, {});
+      const img1 = products.reduce((prev, product) => {
+        const img = product.images[0];
+
+        return {
+          ...prev,
+          [product._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+        };
+      }, {});
+      const img2 = products.reduce((prev, product) => {
+        const img = product.images[1];
+
+        return {
+          ...prev,
+          [product._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+        };
+      }, {});
+      const img3 = products.reduce((prev, product) => {
+        const img = product.images[2];
+        return {
+          ...prev,
+          [product._id]: "https://dark-2.herokuapp.com/img/products/" + img,
+        };
+      }, {});
       return {
         ...newState,
         queryMap: {
           ...state.queryMap,
           [action.payload.query]: productIds,
         },
+        imageCover: { ...state.imageCover, ...imgCover },
+        imageFront: { ...state.imageFront, ...imgFront },
+        image1: { ...state.image1, ...img1 },
+        image2: { ...state.image2, ...img2 },
+        image3: { ...state.image3, ...img3 },
       };
     default:
       return state;
